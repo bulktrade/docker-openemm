209f5d63fdce5d16de3adc7c3ce20c9b1d2aab32
root <root@s3.bulktrade.org>
2016-12-07 11:33:09 +0100

    fix

--- 2015-R3/setup-openemm.sh
+++ 2015-R3/setup-openemm.sh
@@ -37,7 +37,7 @@ then
     mysqladmin -h $MYSQL_PORT_3306_TCP_ADDR -u admin --password=$MYSQL_ENV_MYSQL_PASS create openemm
 	echo -e "[OK]"
 
-	cp openemm-2015_R2.sql openemm-2015.w.sql
+	cp openemm-2015_R3.sql openemm-2015.w.sql
 	sed -i "s/http:\/\/localhost:8080',''/$EOPEN_EMM_URL','$EOPEN_EMM_HOSTNAME'/g" openemm-2015.w.sql
 	sed -i "s/'agnitas'@'localhost'/'agnitas'@'$MYSQL_PORT_3306_TCP_ADDR'/g" openemm-2015.w.sql
 
